b803d0fb28b646e6f20de0178b3e8d98cd25adf6
Rob Moorman <rob@moori.nl>
2018-09-11 19:46:37 +0200

    Initial project setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>App</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
